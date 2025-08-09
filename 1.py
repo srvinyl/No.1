109738990f6af0acb78b7d4d88c8d90ea96be575
User <srv.vinly@gmail.com>
2025-08-09 11:26:01 +0900

    설정 가능한 dictonary key

--- 1.py
+++ 1.py
@@ -10,3 +10,54 @@ print(x[3])
 x = {'a':100, 'b':200}
 print(x)
 print(x['a'])
+example = {
+
+    'python': [True, False, True, True, True, True, True, False, False, True],
+
+    'java': [True, False, False, True, True, True, False, False, False, True],
+
+    'git': [False, False, True, True, False, True, True, True, True, True],
+
+}
+print(example)
+print(example['python']
+print(example['python'][1])
+
+
+python_description = [
+
+    {
+
+        'answer': 1,
+
+        'description': 'python에 대한 설명은 1번이 맞습니다.'
+
+    },
+
+    {
+
+        'answer': "list",
+
+        'description': 'python의 열거형 데이터 타입은 list입니다.'
+
+    },
+
+    {
+
+        'answer': True,
+
+        'description': 'python의 LIST 안에 Dictionary를 사용할 수 있습니다.'
+
+    },
+
+]
+print(python_description[0])
+
+
+list_example = [1, "+", 2, "="]
+print(list_example)
+print(list_example[1])
+
+dict_example = {
+1: 'value 1',}
+print(dict_example)
