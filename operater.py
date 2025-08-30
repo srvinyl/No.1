8a8f1c7c40648a76fe34e1ff1a2136198fc6df25
User <srv.vinly@gmail.com>
2025-08-30 09:32:55 +0900

    숫자 연산

---
+++ operater.py
@@ -0,0 +1,27 @@
+x = 10 #대입 연산자
+y =  15
+
+z = x == y
+print("x ==y  : ", z)
+z = x != y
+print("x ==y  : ", z)
+z = x == y
+print("x !=y  : ", z)
+z = x != y
+print("x ==y  : ", z)
+z = x == y
+print("x ==y  : ", z)
+z = x != y
+print("x ==y  : ", z)
+z = x == y
+print("x !=y  : ", z)
+z = x != y
+print("x ==y  : ", z)
+z = x == y
+print("x ==y  : ", z)
+z = x != y
+print("x ==y  : ", z)
+z = x*y
+print("x !=y  : ", z)
+z =x/y
+print("x /y  : ", z)
