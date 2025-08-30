ae540fa6d346d9c48c0c6e873213527c4568e3ca
User <srv.vinly@gmail.com>
2025-08-30 09:42:48 +0900

    문자형태의 연산

--- operater.py
+++ operater.py
@@ -25,3 +25,15 @@ z = x*y
 print("x !=y  : ", z)
 z =x/y
 print("x /y  : ", z)
+
+
+
+str_x = "hello"
+str_y = "python"
+
+str_z = str_x + str_y
+print( "str_x + str_y", str_z)
+
+
+str_z = str_x * 2
+print("str_x * 2", str_z)
