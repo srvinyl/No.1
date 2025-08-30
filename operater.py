2b8414ac5a03e251cd616cb2742da0898425b072
User <srv.vinly@gmail.com>
2025-08-30 10:16:13 +0900

    지원가능여부

--- operater.py
+++ operater.py
@@ -1,3 +1,5 @@
+import total
+
 x = 10 #대입 연산자
 y =  15
 
@@ -37,3 +39,37 @@ print( "str_x + str_y", str_z)
 
 str_z = str_x * 2
 print("str_x * 2", str_z)
+
+
+array_x = [12314, 12344]
+array_y = [123567, 213123]
+
+array_z = array_x + array_y
+print("array_x + array_y : ",array_z)
+
+array_z = array_x * 2
+print("array_x * 2 : ",array_z)
+
+array_z = array_x * array_y[0]
+print("array_x * array_y[0]:",array_z)
+
+report_card = {
+"국어": 3,
+"영어": 6,
+"수학": 2,
+"물리": 4,
+"화학": 2,
+"생명과학": 5,
+}
+
+can_apply = report_card["국어"] < 3 and report_card["수학"] < 3and report_card["영어"] <= 6
+print("지원가능여부 : ", can_apply)
+
+total_score = report_card["국어"] + report_card["수학"] + report_card["영어"] <= 6
+print("3합 6 :", total_scdre )
+
+can_apply = report_card["국어"] < 3 and report_card["수학"] < 3and report_card["영어"] <= 6
+print("지원가능여부 : ", can_apply)
+
+can_apply_class_for_the_gifted = report_card["물리"] == 1 or report_card["화학"] == 1 or report_card["생명과학"] == 1
+print("영재반 지원가능여부 :", can_apply_class_for_the_gifted )
