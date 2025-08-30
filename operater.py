e74f59448e52e0f5fefff6957fef8532988dbaad
User <srv.vinly@gmail.com>
2025-08-30 11:37:23 +0900

    dictionary 가공 하는 방법

--- operater.py
+++ operater.py
@@ -54,26 +54,23 @@ array_z = array_x * array_y[0]
 print("array_x * array_y[0]:",array_z)
 
 report_card = {
-"국어": 3,
-"영어": 6,
-"수학": 2,
-"물리": 4,
-"화학": 2,
-"생명과학": 5,
+    "국어": 3,
+    "영어": 6,
+    "수학": 2,
+    "물리": 4,
+    "화학": 2,
+    "생명과학": 5,
 }
 
-can_apply = report_card["국어"] < 3 and report_card["수학"] < 3and report_card["영어"] <= 6
+can_apply = report_card["국어"] < 3 and report_card["수학"] < 3 and report_card["영어"] <= 6
 print("지원가능여부 : ", can_apply)
 
-total_score = report_card["국어"] + report_card["수학"] + report_card["영어"] <= 6
-print("3합 6 :", total_scdre )
-
-can_apply = report_card["국어"] < 3 and report_card["수학"] < 3and report_card["영어"] <= 6
-print("지원가능여부 : ", can_apply)
+total_score = report_card["국어"] + report_card["수학"] + report_card["영어"]
+print("3합:", total_score)
+print("3합 6 이하 여부:", total_score <= 6)
 
 can_apply_class_for_the_gifted = report_card["물리"] == 1 or report_card["화학"] == 1 or report_card["생명과학"] == 1
-print("영재반 지원가능여부 :", can_apply_class_for_the_gifted )
-
+print("영재반 지원가능여부 :", can_apply_class_for_the_gifted)
 z = 5 + x * 3
 print("5 + x) * 3:", z)
 z =(5 + x) * 3
@@ -105,3 +102,19 @@ print(len(x))
 
 print("배열의 마지막 인덱스========")
 print(len(x)-1)
+
+print("dictionary 예제 =========")
+x['c'] = 10
+print(x)
+print(x['c'])
+print(x['d'])
+
+del x['a']
+print(x)
+
+print(x.keys())
+print(x.values())
+print(x.items())
+
+print(x.clear())
+print(x)
