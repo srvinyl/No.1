1f6749faff5e7e4e131449d0de37d140df932b6c
User <srv.vinly@gmail.com>
2025-11-22 11:21:17 +0900

    if

--- 11/22.py
+++ 11/22.py
@@ -1,15 +1,75 @@
 #def 와 if의 차이.
 #def(인자)를 통한  함수.
 #if 가정문
-#변수 = 저장소. 함수 = 어떤 역할이 부여된 저장소
-
+test = [
+    {
+        'name': 'aaa',
+        'number': 10101,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'bbb',
+        'number': 10102,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'ccc',
+        'number': 10103,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'ddd',
+        'number': 10104,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'eee',
+        'number': 10105,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    }
+]
+test = [[1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+ [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1, 5, 3, 4, 5, 1, 2, 3, 1, 2],
+ [4, 3, 4, 4, 5, 3, 1, 2, 2, 4],
+ [1, 3, 4, 4, 5, 3, 1, 2, 3, 2],
+ [1, 3, 5, 4, 5, 3, 1, 2, 3, 4]];
 a = [1, 5, 2, 4, 5, 3, 1, 2, 3, 4]
 correct_answer = [1, 5, 2, 4, 5, 3, 1, 2, 3, 4]
-
+student_answer = [1, 5, 2, 4, 5, 3, 1, 2, 3, 4]
 for (student, correct) in zip(a, correct_answer):
      print(student , '/' , correct)
 
-#구상:리스트를 만들고 if 문을 통해 correct_answer와 같다면 100점
+#구상: if 문을 통해 correct_answer와 같다면 100점
 # 인덱스를 통해 리스트와 correct의 값을 비교해 값이 다르다면 -10점씩 차감한다.
 #한다는 아이디어 까지는 생각해 봤는데 구상법을 모르겠습니다.
 
+
+score = 0
+for (student, correct) in zip(a, correct_answer):
+    def get_score(student_answer, correct_answer):
+        # 학생의 점수구하기 한 문항당 10점이라 가정.
+        score = 0
+        for (student, correct) in zip(student_answer, correct_answer):
+            if student == correct:
+                score = score + 10
+        return score
+
+
+    score = get_score(student_answer, correct_answer)
+    print(score)
